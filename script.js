5f92157318fb9f7a1986531d8c8b34b9b801c529
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-30 15:09:16 -0400

    SOLVED :)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+/*
+Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
 
+*/
+
+const bill = 275;
+const tip = bill * (15/100);
+const total = bill >= 50 && bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+is ${bill + tip}`) : console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + (bill * (20/100))}`);
+
+// MY SOLUTION IS ABOVE. IT IS SOLVED, BUT COULD HAVE BEEN WRITTEN LIKE THIS:
+
+//const bill = 275;
+//const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+//console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
