8d6e588cbae0040584011218543eaa6cd576c705
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-30 20:03:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,4 @@ is ${bill + tip}`) : console.log(`The bill was ${bill}, the tip was ${tip}, and 
 //const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 
 //console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
